817a405d0f44f5dee3de30cc2553072edb7bfc6f
csjhonathan <jhonathancarv.s@gmail.com>
2023-08-18 14:23:15 -0300

    feat: can get tweets with pagination and ordered by most recently

---
+++ src/app.controller.ts
@@ -0,0 +1,34 @@
+import { Body, Controller, Get, HttpCode, Post, Query } from '@nestjs/common';
+import { AppService } from './app.service';
+import { RegisterUserDto } from './dtos/user.dto';
+import { TweetInputDto } from './dtos/tweet.dto';
+import { Tweet } from './entities/tweet.entity';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHealth(): string {
+    return this.appService.getHealth();
+  }
+
+  @Post('sign-up')
+  @HttpCode(200)
+  signUp(@Body() body: RegisterUserDto): string {
+    return this.appService.signUp(body.username, body.avatar);
+  }
+
+  @Post('/tweets')
+  postTweet(@Body() body: TweetInputDto): string {
+    return this.appService.postTweet(body.username, body.tweet);
+  }
+
+  @Get('/tweets')
+  getTweets(
+    @Query('page')
+    page: number | null,
+  ): Tweet[] {
+    return this.appService.getTweets(page);
+  }
+}
